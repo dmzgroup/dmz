e5ab2264eeebaf94f66f38297f24f709df1361bf
Randall E. Barker <simstorm@mac.com>
2008-12-23 04:00:43 +0800

    adding opengl library support to dmz build tree
    Signed-off-by: DMZ Develoment <github@dmzdev.org>

---
+++ frameworks/render/lmk/lmkOpenGL.lua
@@ -0,0 +1,25 @@
+require "lmkbuild"
+
+local lmk = lmk
+local sys = lmkbuild.system ()
+
+module (...)
+
+function setup ()
+   local libs = nil
+   local libPaths = nil
+   local includePaths = nil
+   if sys == "win32" then
+      libs = "opengl32.lib glu32.lib"
+   elseif sys == "macos" then
+      libs = "-framework OpenGl -framework Carbon"
+   elseif sys == "linux" then
+      libs = "-lGL -lX11 -lGLU -lXmu"
+      libPaths = "-L/usr/X11R6/lib"
+      includePaths ="-I/usr/X11R6/include"
+   else
+   end
+   if libs then lmk.add_vars { localLibs = libs, } end
+   if libPaths then lmk.add_vars { localLibPaths = libPathss, } end
+   if includePaths then lmk.add_vars { localIncludes = includePaths, } end
+end

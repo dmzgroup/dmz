d0d7aa2a126206d6f1531ac4920e3b3f8a95f545
Randall E. Barker <simstorm@mac.com>
2009-07-22 17:36:34 -0700

    MBRA 2.0 wip

---
+++ frameworks/object/plugins/flag/dmzObjectPluginFlagGroup.cpp
@@ -0,0 +1,112 @@
+#include <dmzObjectAttributeMasks.h>
+#include <dmzObjectModule.h>
+#include "dmzObjectPluginFlagGroup.h"
+#include <dmzRuntimeConfigToTypesBase.h>
+#include <dmzRuntimePluginFactoryLinkSymbol.h>
+#include <dmzRuntimePluginInfo.h>
+
+namespace {
+
+static dmz::Int32 LocalData = 0;
+
+};
+
+dmz::ObjectPluginFlagGroup::ObjectPluginFlagGroup (
+      const PluginInfo &Info,
+      Config &local) :
+      Plugin (Info),
+      ObjectObserverUtil (Info, local),
+      _log (Info),
+      _maxGroupSize (1),
+      _flagAttrHandle (0) {
+
+   _init (local);
+}
+
+
+dmz::ObjectPluginFlagGroup::~ObjectPluginFlagGroup () {
+
+   _flagGroup.clear ();
+}
+
+
+// Object Observer Interface
+void
+dmz::ObjectPluginFlagGroup::destroy_object (
+      const UUID &Identity,
+      const Handle ObjectHandle) {
+
+   _flagGroup.remove (ObjectHandle);
+}
+
+
+void
+dmz::ObjectPluginFlagGroup::remove_object_attribute (
+      const UUID &Identity,
+      const Handle ObjectHandle,
+      const Handle AttributeHandle,
+      const Mask &AttrMask) {
+
+   if (AttrMask.contains (ObjectFlagMask)) {
+
+      _flagGroup.remove (ObjectHandle);
+   }
+}
+
+
+void
+dmz::ObjectPluginFlagGroup::update_object_flag (
+      const UUID &Identity,
+      const Handle ObjectHandle,
+      const Handle AttributeHandle,
+      const Boolean Value,
+      const Boolean *PreviousValue) {
+
+   if (Value) {
+
+      _flagGroup.store (ObjectHandle, &LocalData);
+
+      if (_flagGroup.get_count () > _maxGroupSize) {
+
+         HashTableHandleIterator it;
+
+         if (_flagGroup.get_first (it)) {
+
+            ObjectModule *objMod (get_object_module ());
+
+            if (objMod) {
+
+               objMod->store_flag (it.get_hash_key (), _flagAttrHandle, False);
+            }
+         }
+      }
+   }
+   else { _flagGroup.remove (ObjectHandle); }
+}
+
+
+void
+dmz::ObjectPluginFlagGroup::_init (Config &local) {
+
+   _maxGroupSize = config_to_int32 ("group.size", local, _maxGroupSize);
+
+   activate_default_object_attribute (ObjectDestroyMask);
+
+   _flagAttrHandle = activate_object_attribute (
+      config_to_string ("flag.attribute", local),
+      ObjectRemoveAttributeMask | ObjectFlagMask);
+}
+
+
+extern "C" {
+
+DMZ_PLUGIN_FACTORY_LINK_SYMBOL dmz::Plugin *
+create_dmzObjectPluginFlagGroup (
+      const dmz::PluginInfo &Info,
+      dmz::Config &local,
+      dmz::Config &global) {
+
+   return new dmz::ObjectPluginFlagGroup (Info, local);
+}
+
+};
---
+++ frameworks/object/plugins/flag/dmzObjectPluginFlagGroup.h
@@ -0,0 +1,61 @@
+#ifndef DMZ_OBJECT_PLUGIN_FLAG_GROUP_DOT_H
+#define DMZ_OBJECT_PLUGIN_FLAG_GROUP_DOT_H
+
+#include <dmzObjectObserverUtil.h>
+#include <dmzRuntimeLog.h>
+#include <dmzRuntimePlugin.h>
+#include <dmzTypesHashTableHandleTemplate.h>
+
+namespace dmz {
+
+   class ObjectPluginFlagGroup :
+         public Plugin,
+         public ObjectObserverUtil {
+
+      public:
+         ObjectPluginFlagGroup (const PluginInfo &Info, Config &local);
+         ~ObjectPluginFlagGroup ();
+
+         // Plugin Interface
+         virtual void update_plugin_state (
+            const PluginStateEnum State,
+            const UInt32 Level) {;}
+
+         virtual void discover_plugin (
+            const PluginDiscoverEnum Mode,
+            const Plugin *PluginPtr) {;}
+
+         // Object Observer Interface
+         virtual void destroy_object (const UUID &Identity, const Handle ObjectHandle);
+
+         virtual void remove_object_attribute (
+            const UUID &Identity,
+            const Handle ObjectHandle,
+            const Handle AttributeHandle,
+            const Mask &AttrMask);
+
+         virtual void update_object_flag (
+            const UUID &Identity,
+            const Handle ObjectHandle,
+            const Handle AttributeHandle,
+            const Boolean Value,
+            const Boolean *PreviousValue);
+
+      protected:
+         void _init (Config &local);
+
+         Log _log;
+
+         Int32 _maxGroupSize;
+         Handle _flagAttrHandle;
+         HashTableHandleTemplate<Int32> _flagGroup;
+
+      private:
+         ObjectPluginFlagGroup ();
+         ObjectPluginFlagGroup (const ObjectPluginFlagGroup &);
+         ObjectPluginFlagGroup &operator= (const ObjectPluginFlagGroup &);
+
+   };
+};
+
+#endif // DMZ_OBJECT_PLUGIN_FLAG_GROUP_DOT_H

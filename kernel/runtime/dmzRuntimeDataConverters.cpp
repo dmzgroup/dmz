f643b098889f82c54e8ec20d94294b45429f9737
Randall E. Barker <simstorm@mac.com>
2010-01-16 12:12:05 -0800

    Moved all DataConverter cold into one file.

--- kernel/runtime/dmzRuntimeDataConverters.cpp
+++ kernel/runtime/dmzRuntimeDataConverters.cpp
@@ -1,7 +1,8 @@
-#include <dmzRuntimeDataConverterTypesBase.h>
-#include <dmzRuntimeDefinitions.h>
-#include <dmzRuntimePluginInfo.h>
 #include "dmzRuntimeDataConverterCommon.h"
+#include <dmzRuntimeDataConverterStringContainer.h>
+#include <dmzRuntimeDataConverterTypesBase.h>
+#include <dmzRuntimeDataConverterUUID.h>
+#include <dmzRuntimePluginInfo.h>
 
 /*!
 
@@ -575,4 +576,313 @@ dmz::DataConverterHandle::write_data (
    if (_state.handle) { data.store_uint32 (_state.handle, Element, Value); }
 }
 
+
+/*!
+
+\class dmz::DataConverterUUID
+\ingroup Runtime
+\brief Class for converting between a UUID and a Data object.
+\note This class does not serialize a Data object into a UUID. It provides
+a simple way to package a string into a Data object and then extract that UUID.
+
+*/
+
+struct dmz::DataConverterUUID::State : public DataConverterCommonStruct {
+
+   State (RuntimeContext *context) {
+
+      set_handle ("UUID", context);
+   }
+};
+
+
+/*!
+
+\brief Constructor.
+\param[in] Info reference to PluginInfo.
+
+*/
+dmz::DataConverterUUID::DataConverterUUID (const PluginInfo &Info) :
+      _state (*(new State (Info.get_context ()))) {;}
+
+
+/*!
+
+\brief Constructor.
+\param[in] context Pointer to the runtime context.
+
+*/
+dmz::DataConverterUUID::DataConverterUUID (RuntimeContext *context) :
+      _state (*(new State (context))) {;}
+
+
+//! Destructor.
+dmz::DataConverterUUID::~DataConverterUUID () { delete &_state; }
+
+
+/*!
+
+\brief Sets the attribute handle used to store and retrieve the value in the Data object.
+\param[in] HandleName String containing the name of the attribute handle.
+\param[in] context Pointer to the runtime context.
+\return Returns the Handle being used.
+
+*/
+dmz::Handle
+dmz::DataConverterUUID::set_handle (
+      const String &HandleName,
+      RuntimeContext *context) {
+
+   return _state.set_handle (HandleName, context);
+}
+
+
+/*!
+
+\brief Converts Data object to a UUID.
+\note This function assumes the UUID in the Data object was stored
+using the dmz::DataConverterUUID::to_data function.
+\param[in] Value Data object to convert to a UUID.
+\param[in] Element Index of attribute element.
+\return Returns UUID containing converted data. The UUID will be invalid if
+the Data object is not the correct type or is empty.
+
+*/
+dmz::UUID
+dmz::DataConverterUUID::to_uuid (const Data &Value, const Int32 Element) {
+
+   UUID result;
+
+   Value.lookup_uuid (_state.handle, Element, result);
+
+   return result;
+}
+
+
+/*!
+
+\brief Converts Pointer to a Data object to a UUID.
+\note This function assumes the UUID in the Data object was stored
+using the dmz::DataConverterUUID::to_data function.
+\param[in] Value Pointer to the Data object to convert to a UUID.
+\param[in] Element Index of attribute element.
+\return Returns UUID containing converted data. The UUID will be invalid if
+the Data object is not the correct type or is empty.
+
+*/
+dmz::UUID
+dmz::DataConverterUUID::to_uuid (const Data *Value, const Int32 Element) {
+
+   UUID result;
+
+   if (Value) { result = to_uuid (*Value, Element); }
+
+   return result;
+}
+
+
+/*!
+
+\brief Converts a UUID to a Data object.
+\param[in] Value UUID containing value to store in returned Data object.
+\return Returns a Data object containing the UUID in \a Value.
+
+*/
+dmz::Data
+dmz::DataConverterUUID::to_data (const UUID &Value) {
+
+   Data result (_state.context);
+
+   if (_state.handle) { result.store_uuid (_state.handle, 0, Value); }
+
+   return result;
+}
+
+
+/*!
+
+\brief Writes a UUID to a Data object.
+\param[in] Value UUID to store in the Data object.
+\param[out] data Data object used to store the \a Value
+\param[in] Element Index of attribute element.
+
+*/
+void
+dmz::DataConverterUUID::write_data (
+      const UUID &Value,
+      Data &data,
+      const Int32 Element) {
+
+   if (_state.handle) { data.store_uuid (_state.handle, Element, Value); }
+}
+
+
+/*!
+
+\class dmz::DataConverterStringContainer
+\ingroup Runtime
+\brief Class for converting between a String and a Data object.
+\note This class does not serialize a Data object into a String. It provides
+a simple way to package a string into a Data object and then extract that String.
+
+*/
+
+struct dmz::DataConverterStringContainer::State : public DataConverterCommonStruct {
+
+   State (RuntimeContext *context)  {
+
+      set_handle (base_type_enum_to_string (BaseTypeString), context);
+   }
+};
+
+
+/*!
+
+\brief Constructor.
+\param[in] Info reference to PluginInfo.
+
+*/
+dmz::DataConverterStringContainer::DataConverterStringContainer (const PluginInfo &Info) :
+      _state (*(new State (Info.get_context ()))) {;}
+
+
+/*!
+
+\brief Constructor.
+\param[in] context Pointer to the runtime context.
+
+*/
+dmz::DataConverterStringContainer::DataConverterStringContainer (RuntimeContext *context) :
+      _state (*(new State (context))) {;}
+
+
+//! Destructor.
+dmz::DataConverterStringContainer::~DataConverterStringContainer () { delete &_state; }
+
+
+/*!
+
+\brief Sets the attribute handle used to store and retrieve the value in the Data object.
+\param[in] HandleName StringContainer containing the name of the attribute handle.
+\param[in] context Pointer to the runtime context.
+\return Returns the Handle being used.
+
+*/
+dmz::Handle
+dmz::DataConverterStringContainer::set_handle (
+      const String &HandleName,
+      RuntimeContext *context) {
+
+   return _state.set_handle (HandleName, context);
+}
+
+
+/*!
+
+\brief Converts Data object to a StringContainer.
+\note This function assumes the StringContainer in the Data object was stored
+using the dmz::DataConverterStringContainer::to_data function.
+\param[in] Value Data object to convert to a StringContainer.
+\return Returns StringContainer containing converted data. The StringContainer will be empty if
+the Data object is not the correct type or is empty.
+
+*/
+dmz::StringContainer
+dmz::DataConverterStringContainer::to_string_container (const Data &Value) {
+
+   StringContainer result;
+
+   const Int32 Count = Value.lookup_attribute_element_count (_state.handle);
+
+   for (Int32 ix = 0; ix < Count; ix++) {
+
+      String str;
+      Value.lookup_string (_state.handle, ix, str);
+      result.append (str);
+   }
+
+   return result;
+}
+
+
+/*!
+
+\brief Converts Pointer to a Data object to a StringContainer.
+\note This function assumes the StringContainer in the Data object was stored
+using the dmz::DataConverterStringContainer::to_data function.
+\param[in] Value Pointer to the Data object to convert to a StringContainer.
+\return Returns StringContainer containing converted data. The StringContainer will be empty if
+the Data object is not the correct type or is empty.
+
+*/
+dmz::StringContainer
+dmz::DataConverterStringContainer::to_string_container (const Data *Value) {
+
+   StringContainer result;
+
+   if (Value) { result = to_string_container (*Value); }
+
+   return result;
+}
+
+
+/*!
+
+\brief Converts a StringContainer to a Data object.
+\param[in] Value StringContainer containing value to store in returned Data object.
+\return Returns a Data object containing the string in \a Value.
+
+*/
+dmz::Data
+dmz::DataConverterStringContainer::to_data (const StringContainer &Value) {
+
+   Data result (_state.context);
+
+   write_data (Value, result);
+
+   return result;
+}
+
+
+/*!
+
+\brief Converts a StringContainer pointer to a Data object.
+\param[in] Value StringContainer pointer containing value to store in returned Data object.
+\return Returns a Data object containing the string in \a Value.
+
+*/
+dmz::Data
+dmz::DataConverterStringContainer::to_data (const StringContainer *Value) {
+
+   Data result (_state.context);
+
+   if (Value) { result = to_data (*Value); }
+
+   return result;
+}
+
+
+/*!
+
+\brief Writes a StringContainer to a Data object.
+\param[in] Value StringContainer to store in the Data object.
+\param[out] data Data object used to store the \a Value
+
+*/
+void
+dmz::DataConverterStringContainer::write_data (const StringContainer &Value, Data &data) {
+
+   if (_state.handle) {
+
+      Int32 count (0);
+      StringContainerIterator it;
+      String str;
+
+      while (Value.get_next (it, str)) {
+
+         data.store_string (_state.handle, count, str);
+         count++;
+      }
+   }
+}
 

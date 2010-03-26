15cb0e6dd0d35ddcce5ca1d79735a1c69aba5e96
Randall E. Barker <simstorm@mac.com>
2010-03-25 17:36:48 -0700

    Event Render plugin WIP

---
+++ frameworks/render/plugins/osg/dmzRenderPluginEventOSG.cpp
@@ -0,0 +1,339 @@
+#include <dmzEventCallbackMasks.h>
+#include <dmzEventConsts.h>
+#include <dmzEventModule.h>
+#include <dmzRenderModuleCoreOSG.h>
+#include "dmzRenderPluginEventOSG.h"
+#include <dmzRenderUtilOSG.h>
+#include <dmzRuntimeConfigToTypesBase.h>
+#include <dmzRuntimeEventType.h>
+#include <dmzRuntimePluginFactoryLinkSymbol.h>
+#include <dmzRuntimePluginInfo.h>
+
+#include <osg/Billboard>
+#include <osg/CullFace>
+#include <osg/Material>
+#include <osg/Geode>
+#include <osg/Geometry>
+#include <osg/BlendFunc>
+#include <osg/Depth>
+#include <osg/PolygonOffset>
+#include <osg/Matrix>
+#include <osg/RenderInfo>
+#include <osg/Texture2D>
+         
+#include <osgDB/ReadFile>
+
+dmz::RenderPluginEventOSG::RenderPluginEventOSG (const PluginInfo &Info, Config &local) :
+      Plugin (Info),
+      TimeSlice (Info),
+      EventObserverUtil (Info, local),
+      _log (Info),
+      _defs (Info),
+      _rc (Info),
+      _core (0),
+      _defaultHandle (0) {
+
+   _init (local);
+}
+
+
+dmz::RenderPluginEventOSG::~RenderPluginEventOSG () {
+
+}
+
+
+// Plugin Interface
+void
+dmz::RenderPluginEventOSG::update_plugin_state (
+      const PluginStateEnum State,
+      const UInt32 Level) {
+
+   if (State == PluginStateInit) {
+
+   }
+   else if (State == PluginStateStart) {
+
+   }
+   else if (State == PluginStateStop) {
+
+   }
+   else if (State == PluginStateShutdown) {
+
+   }
+}
+
+
+void
+dmz::RenderPluginEventOSG::discover_plugin (
+      const PluginDiscoverEnum Mode,
+      const Plugin *PluginPtr) {
+
+   if (Mode == PluginDiscoverAdd) {
+
+      if (!_core) { _core = RenderModuleCoreOSG::cast (PluginPtr); }
+   }
+   else if (Mode == PluginDiscoverRemove) {
+
+      if (_core &&  (_core == RenderModuleCoreOSG::cast (PluginPtr))) { _core = 0; }
+   }
+}
+
+
+// TimeSlice Interface
+void
+dmz::RenderPluginEventOSG::update_time_slice (const Float64 TimeDelta) {
+
+   HashTableHandleIterator it;
+   EventStruct *event (0);
+   osg::Group *group = _core ? _core->get_static_objects () : 0;
+
+   while (_eventTable.get_next (it, event)) {
+
+      event->scalar += event->Type.Rate * TimeDelta;
+
+      if (event->scalar > event->Type.Scale) {
+
+         if (group) { group->removeChild (event->root.get ()); }
+
+         event = _eventTable.remove (it.get_hash_key ());
+
+         if (event) { delete event; event = 0; }
+      }
+      else {
+
+         const Float64 Value = event->scalar;
+         osg::Matrix mat;
+         mat.makeScale (osg::Vec3 (Value, Value, Value));
+         event->scale->setMatrix (mat);
+      }
+   }
+}
+
+
+// Event Observer Interface
+void
+dmz::RenderPluginEventOSG::close_event (
+      const Handle EventHandle,
+      const EventType &Type,
+      const EventLocalityEnum Locality) {
+
+   const Handle TypeHandle = Type.get_handle ();
+
+   if (!_ignore.contains (TypeHandle)) {
+
+      TypeStruct *ts = _get_type (Type);
+
+      if (ts) {
+
+         EventStruct *es = new EventStruct (*ts);
+
+         if (es) { _create_event (EventHandle, *es); }
+      }
+   }
+}
+
+
+// RenderPluginEventOSG Interface
+dmz::RenderPluginEventOSG::TypeStruct *
+dmz::RenderPluginEventOSG::_get_type (const EventType &Type) {
+
+   TypeStruct *result (0);
+   EventType current (Type);
+   Boolean created (False);
+
+   while (current && !result) {
+
+      result = _typeMap.lookup (current.get_handle ());
+
+      if (!result) { result = _create_type (current); }
+
+      if (!result) { current.become_parent (); }
+   }
+
+   if (result && (current != Type)) {
+
+      _typeMap.store (Type.get_handle (), result);
+   }
+
+   return result;
+}
+
+
+dmz::RenderPluginEventOSG::TypeStruct *
+dmz::RenderPluginEventOSG::_create_type (const EventType &Type) {
+
+   TypeStruct *result (0);
+
+   Config info = Type.get_config ();
+
+   if (info) {
+
+      const String ImageRc = config_to_string ("render.image.resource", info);
+      const Float64 Offset = config_to_float64 ("render.offset.value", info, 2);
+      const Float64 Scale = config_to_float64 ("render.scale.value", info, 1.1);
+      const Float64 Rate = config_to_float64 ("render.scale.rate", info, 0.1);
+
+      if (ImageRc) {
+
+         const String ImageName = _rc.find_file (ImageRc);
+
+         if (ImageName) {
+
+            result = new TypeStruct (Offset, Scale, Rate);
+
+            if (result) {
+
+               result->image = osgDB::readImageFile (ImageName.get_buffer ());
+
+               if (result->image.valid ()) {
+
+                  if (_typeTable.store (Type.get_handle (), result)) {
+
+                     _typeMap.store (Type.get_handle (), result);
+                  }
+                  else { delete result; result = 0; }
+               }
+               else {
+
+                  _log.error << "Failed to load image resource: " << ImageRc
+                     << " -> " << ImageName << endl;
+                  _ignore.add_handle (Type.get_handle ());
+               }
+            }
+         }
+         else {
+
+            _log.error << "Failed to find image resource: " << ImageRc << endl;
+            _ignore.add_handle (Type.get_handle ());
+         }
+      }
+   }
+
+   return result;
+}
+
+
+void
+dmz::RenderPluginEventOSG::_create_event (const Handle EventHandle, EventStruct &event) {
+
+   event.root = new osg::MatrixTransform;
+   event.scale = new osg::MatrixTransform;
+   event.root->addChild (event.scale);
+
+   EventModule *module = get_event_module ();
+
+   if (module) {
+
+      Vector pos;
+      module->lookup_position (EventHandle, _defaultHandle, pos);
+      osg::Matrix mat;
+      mat.makeTranslate (to_osg_vector (pos));
+      event.root->setMatrix (mat);
+   }
+
+   osg::Billboard* geode = new osg::Billboard ();
+
+   osg::Geometry* geom = new osg::Geometry;
+
+   osg::Vec4Array* colors = new osg::Vec4Array;
+   colors->push_back (osg::Vec4 (1.0f, 1.0f, 1.0f, 1.0f));
+   geom->setColorArray (colors);
+   geom->setColorBinding (osg::Geometry::BIND_OVERALL);
+
+   osg::StateSet *stateset = geom->getOrCreateStateSet ();
+   stateset->setMode (GL_BLEND, osg::StateAttribute::ON);
+
+   osg::Texture2D *tex = new osg::Texture2D (event.Type.image.get ());
+   tex->setWrap (osg::Texture2D::WRAP_S, osg::Texture2D::REPEAT);
+   tex->setWrap (osg::Texture2D::WRAP_T, osg::Texture2D::REPEAT);
+
+   stateset->setTextureAttributeAndModes (0, tex, osg::StateAttribute::ON);
+
+   stateset->setAttributeAndModes (new osg::CullFace (osg::CullFace::BACK));
+
+   const float Off (event.Type.Offset);
+
+   osg::Vec3 v1 (-Off, 0.0, -Off);
+   osg::Vec3 v2 ( Off, 0.0, -Off);
+   osg::Vec3 v3 ( Off, 0.0, -Off);
+   osg::Vec3 v4 (-Off, 0.0, -Off);
+
+   osg::Vec3 n1 ( 1.0,  1.0,  0.0);
+   n1.normalize ();
+
+   const float F1 (1.0f);
+   const float F2 (1.0f);
+
+   osg::Vec3Array *vertices = new osg::Vec3Array;
+   osg::Vec2Array *tcoords = new osg::Vec2Array;
+   osg::Vec3Array* normals = new osg::Vec3Array;
+
+   vertices->push_back (v1);
+   vertices->push_back (v2);
+   vertices->push_back (v3);
+   vertices->push_back (v4);
+   tcoords->push_back (osg::Vec2 (F1, F1));
+   tcoords->push_back (osg::Vec2 (F1, F2));
+   tcoords->push_back (osg::Vec2 (F2, F2));
+   tcoords->push_back (osg::Vec2 (F2, F1));
+   normals->push_back (n1);
+
+   geom->setNormalArray (normals);
+   geom->setNormalBinding (osg::Geometry::BIND_OVERALL);
+   geom->addPrimitiveSet (new osg::DrawArrays (GL_QUADS, 0, 4));
+   geom->setVertexArray (vertices);
+   geom->setTexCoordArray (0, tcoords);
+   geode->addDrawable (geom);
+
+   event.scale->addChild (geode);
+
+   if (_eventTable.store (EventHandle, &event)) {
+
+      osg::Group *group = _core ? _core->get_static_objects () : 0;
+
+      if (group) { group->addChild (event.root.get ()); }
+      else { _log.error << "Failed to add geode!" << endl; }
+   }
+   else { delete &event; }
+}
+
+
+void
+dmz::RenderPluginEventOSG::_init (Config &local) {
+
+   RuntimeContext *context = get_plugin_runtime_context ();
+
+   EventTypeSet set = config_to_event_type_set ("events", local, context);
+
+   if (set.get_count () == 0) {
+
+      EventType root = _defs.get_root_event_type ();
+      activate_event_callback (root, EventCloseMask);
+   }
+   else {
+
+      EventTypeIterator it;
+      EventType type;
+
+      while (set.get_next (it, type)) {
+
+         activate_event_callback (type, EventCloseMask);
+      }
+   }
+
+   _defaultHandle = _defs.create_named_handle (EventAttributeDefaultName);
+}
+
+
+extern "C" {
+
+DMZ_PLUGIN_FACTORY_LINK_SYMBOL dmz::Plugin *
+create_dmzRenderPluginEventOSG (
+      const dmz::PluginInfo &Info,
+      dmz::Config &local,
+      dmz::Config &global) {
+
+   return new dmz::RenderPluginEventOSG (Info, local);
+}
+
+};
---
+++ frameworks/render/plugins/osg/dmzRenderPluginEventOSG.h
@@ -0,0 +1,104 @@
+#ifndef DMZ_RENDER_PLUGIN_EVENT_OSG_DOT_H
+#define DMZ_RENDER_PLUGIN_EVENT_OSG_DOT_H
+
+#include <dmzEventObserverUtil.h>
+#include <dmzRuntimeDefinitions.h>
+#include <dmzRuntimeLog.h>
+#include <dmzRuntimePlugin.h>
+#include <dmzRuntimeResources.h>
+#include <dmzRuntimeTimeSlice.h>
+#include <dmzTypesHandleContainer.h>
+#include <dmzTypesHashTableHandleTemplate.h>
+
+#include <osg/Image>
+#include <osg/MatrixTransform>
+#include <osg/ref_ptr>
+
+namespace dmz {
+
+   class EventType;
+   class RenderModuleCoreOSG;
+
+   class RenderPluginEventOSG :
+         public Plugin,
+         public TimeSlice,
+         public EventObserverUtil {
+
+      public:
+         RenderPluginEventOSG (const PluginInfo &Info, Config &local);
+         ~RenderPluginEventOSG ();
+
+         // Plugin Interface
+         virtual void update_plugin_state (
+            const PluginStateEnum State,
+            const UInt32 Level);
+
+         virtual void discover_plugin (
+            const PluginDiscoverEnum Mode,
+            const Plugin *PluginPtr);
+
+         // TimeSlice Interface
+         virtual void update_time_slice (const Float64 TimeDelta);
+
+         // Event Observer Interface
+         virtual void close_event (
+            const Handle EventHandle,
+            const EventType &Type,
+            const EventLocalityEnum Locality);
+
+      protected:
+         struct TypeStruct {
+
+            const Float64 Offset;
+            const Float64 Scale;
+            const Float64 Rate;
+            osg::ref_ptr<osg::Image> image;
+
+            TypeStruct (
+                  const Float64 TheOffset,
+                  const Float64 TheScale,
+                  const Float64 TheRate) :
+                  Offset (TheOffset),
+                  Scale (TheScale),
+                  Rate (TheRate) {;}
+         };
+
+         struct EventStruct {
+
+            const TypeStruct &Type;
+
+            Float64 scalar;
+            osg::ref_ptr<osg::MatrixTransform> root;
+            osg::ref_ptr<osg::MatrixTransform> scale;
+
+            EventStruct (const TypeStruct &TheType) : Type (TheType), scalar (1.0) {;}
+         };
+
+         TypeStruct *_get_type (const EventType &Type);
+         TypeStruct *_create_type (const EventType &Type);
+         void _create_event (const Handle EventHandle, EventStruct &event);
+         void _init (Config &local);
+
+         Log _log;
+         Definitions _defs;
+         Resources _rc;
+
+         RenderModuleCoreOSG *_core;
+
+         Handle _defaultHandle;
+
+         HandleContainer _ignore;
+
+         HashTableHandleTemplate<TypeStruct> _typeMap;
+         HashTableHandleTemplate<TypeStruct> _typeTable;
+
+         HashTableHandleTemplate<EventStruct> _eventTable;
+
+      private:
+         RenderPluginEventOSG ();
+         RenderPluginEventOSG (const RenderPluginEventOSG &);
+         RenderPluginEventOSG &operator= (const RenderPluginEventOSG &);
+   };
+};
+
+#endif // DMZ_RENDER_PLUGIN_EVENT_OSG_DOT_H

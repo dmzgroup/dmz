c8081391ee9dc533c0bd57117c597b332bdd5dea
Randall E. Barker <simstorm@mac.com>
2010-08-10 15:55:43 -0700

    Updated to latest version of yajl

--- foundation/3rdparty/yajl/yajl_parser.c
+++ foundation/3rdparty/yajl/yajl_parser.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2007-2009, Lloyd Hilaiel.
+ * Copyright 2010, Lloyd Hilaiel.
  * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -48,7 +48,7 @@ unsigned char *
 yajl_render_error_string(yajl_handle hand, const unsigned char * jsonText,
                          unsigned int jsonTextLen, int verbose)
 {
-    unsigned int offset = hand->errorOffset;
+    unsigned int offset = hand->bytesConsumed;
     unsigned char * str;
     const char * errorType = NULL;
     const char * errorText = NULL;
@@ -136,12 +136,16 @@ yajl_render_error_string(yajl_handle hand, const unsigned char * jsonText,
 
 
 yajl_status
-yajl_do_parse(yajl_handle hand, unsigned int * offset,
-              const unsigned char * jsonText, unsigned int jsonTextLen)
+yajl_do_parse(yajl_handle hand, const unsigned char * jsonText,
+              unsigned int jsonTextLen)
 {
     yajl_tok tok;
     const unsigned char * buf;
     unsigned int bufLen;
+    unsigned int * offset = &(hand->bytesConsumed);
+
+    *offset = 0;
+    
 
   around_again:
     switch (yajl_bs_current(hand->stateStack)) {
@@ -149,7 +153,6 @@ yajl_do_parse(yajl_handle hand, unsigned int * offset,
             return yajl_status_ok;
         case yajl_state_lexical_error:
         case yajl_state_parse_error:            
-            hand->errorOffset = *offset;
             return yajl_status_error;
         case yajl_state_start:
         case yajl_state_map_need_val:
